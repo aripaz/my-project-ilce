68cdbc323a70a17bfba0a4192b04db499878f870
carlos.catrilef <carlos.catrilef@e6c9a562-5fef-0af6-cc1b-9fddd166eedc>
2011-04-15 22:12:02 +0000

    Se agregaron nuevos campos y metodos al Bean, para contener datos de la aplicacion

--- ProyILCE/src/java/mx/ilce/controller/Aplicacion.java
+++ ProyILCE/src/java/mx/ilce/controller/Aplicacion.java
@@ -5,14 +5,72 @@
 
 package mx.ilce.controller;
 
-import java.math.BigDecimal;
 import mx.ilce.handler.ExecutionHandler;
 
 /**
- *
+ *  Clase pra la implementacion de los metodos asociados a la aplicacion
  * @author ccatrilef
  */
 public class Aplicacion extends Entidad {
+
+    private Integer claveAplicacion;
+    private String aplicacion;
+    private Integer claveFormaPrincipal;
+    private String descripcion;
+    private String aliasMenuNuevaEntidad;
+    private String aliasMenuMostrarEntidad;
+
+/********* GETTER Y SETTER *********/
+
+    public String getAliasMenuMostrarEntidad() {
+        return aliasMenuMostrarEntidad;
+    }
+
+    public void setAliasMenuMostrarEntidad(String aliasMenuMostrarEntidad) {
+        this.aliasMenuMostrarEntidad = aliasMenuMostrarEntidad;
+    }
+
+    public String getAliasMenuNuevaEntidad() {
+        return aliasMenuNuevaEntidad;
+    }
+
+    public void setAliasMenuNuevaEntidad(String aliasMenuNuevaEntidad) {
+        this.aliasMenuNuevaEntidad = aliasMenuNuevaEntidad;
+    }
+
+    public String getAplicacion() {
+        return aplicacion;
+    }
+
+    public void setAplicacion(String aplicacion) {
+        this.aplicacion = aplicacion;
+    }
+
+    public Integer getClaveAplicacion() {
+        return claveAplicacion;
+    }
+
+    public void setClaveAplicacion(Integer claveAplicacion) {
+        this.claveAplicacion = claveAplicacion;
+    }
+
+    public Integer getClaveFormaPrincipal() {
+        return claveFormaPrincipal;
+    }
+
+    public void setClaveFormaPrincipal(Integer claveFormaPrincipal) {
+        this.claveFormaPrincipal = claveFormaPrincipal;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+/******* OPERACIONES DE ENTIDAD ******/
 
     public ExecutionHandler ingresarEntidad(Object data) {
         throw new UnsupportedOperationException("Not supported yet.");
