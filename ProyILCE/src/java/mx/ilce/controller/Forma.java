7ca3b5a6898587c9d4085180895c184e60b36d43
carlos.catrilef <carlos.catrilef@e6c9a562-5fef-0af6-cc1b-9fddd166eedc>
2011-04-26 17:05:25 +0000

    Se agregaron metodos para el manejo de las formas, a partir de las aplicaciones. Se prevee dejar en memoria este objeto para mantener las formas a partir de las aplicaciones asociadas al Perfil del usuario, y no buscar nuevamente en la BD

--- ProyILCE/src/java/mx/ilce/controller/Forma.java
+++ ProyILCE/src/java/mx/ilce/controller/Forma.java
@@ -5,11 +5,17 @@
 
 package mx.ilce.controller;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import mx.ilce.component.AdminXML;
+import mx.ilce.conection.ConEntidad;
 import mx.ilce.handler.ExecutionHandler;
 
 /**
- *  Clase para la implementacion de los metodos asociados a Forma
+ * Clase para la implementacion de los metodos asociados a Forma
+ * Se prevee dejar en memoria esta objeto para que mantenga las formas
+ * obtenidas a partir de las aplicaciones asociadas al Perfil del usuario
  *
  * @author ccatrilef
  */
@@ -20,8 +26,9 @@ public class Forma extends Entidad{
     private Integer claveAplicacion;
     private Integer claveForma;
     private Integer claveFormaPadre;
+    private HashMap hsForma;
 
-/************** GETTER Y SETTER ***************/
+    /************** GETTER Y SETTER ***************/
 
     public String getAliasTab() {
         return aliasTab;
@@ -61,6 +68,42 @@ public class Forma extends Entidad{
 
     public void setOrdenTab(Integer ordenTab) {
         this.ordenTab = ordenTab;
+    }
+
+    public HashMap getHsForma() {
+        return hsForma;
+    }
+
+    public void setHsForma(HashMap hsForma) {
+        this.hsForma = hsForma;
+    }
+
+    /**
+     * Agrega una forma (con formato List) al Hash de formas, asignandole como
+     * Key, el ID de la Forma. Si existia una forma con la misma Key, esta es 
+     * reemplazada
+     * @param key   ID de la Forma
+     * @param obj   Listado de la forma a agregar
+     */
+    public void addForma(Integer key, List obj){
+        this.hsForma.put(key, obj);
+    }
+
+    /**
+     * Obtiene una forma desde el listado de formas, mediante el ID introducido
+     * como Key
+     * @param key   ID de la forma a solicitar
+     * @return
+     */
+    public List getForma(Integer key){
+        List hs = (List) this.hsForma.get(key);
+        return hs;
+    }
+
+    public Forma() {
+        this.aliasTab = "";
+        //this.lstFormas = new ArrayList();
+        this.hsForma = new HashMap();
     }
 
 /************** METODOS ENTIDAD ****************/
@@ -108,37 +151,6 @@ public class Forma extends Entidad{
         try{
             //procesar el xml como se necesite y asignarlo
             StringBuffer str = new StringBuffer("<?xml version='1.0' encoding='ISO-8859-1'?>");
-            str.append("<rows>");
-            str.append("<page>1</page>");
-            str.append("<total>1</total>");
-            str.append("<records>5</records>");
-            str.append("<row id='1'>");
-            str.append("<cell>1</cell>");
-            str.append("<cell>Back End</cell>");
-            str.append("<cell><![CDATA[En desarrollo]]></cell>");
-            str.append("</row>");
-            str.append("<row id='2'>");
-            str.append("<cell>2</cell>");
-            str.append("<cell>CRM</cell>");
-            str.append("<cell><![CDATA[En desarrollo]]></cell>");
-            str.append("</row>	");
-            str.append("<row id='3'>");
-            str.append("<cell>3</cell>");
-            str.append("<cell>Proyectos</cell>");
-            str.append("<cell><![CDATA[En desarrollo]]></cell>");
-            str.append("</row>	");
-            str.append("<row id='4'>");
-            str.append("<cell>4</cell>");
-            str.append("<cell>SRM</cell>");
-            str.append("<cell><![CDATA[En desarrollo]]></cell>");
-            str.append("</row>	");
-            str.append("<row id='5'>");
-            str.append("<cell>5</cell>");
-            str.append("<cell>Control presupuestal</cell>");
-            str.append("<cell><![CDATA[En desarrollo]]></cell>");
-            str.append("</row>");
-            str.append("</rows>");
-            
             this.setXmlEntidad(str);
             AdminXML admXml = new AdminXML();
             //this.setData(admXml.getDataByXML(this.getXmlEntidad()));
@@ -215,5 +227,54 @@ public class Forma extends Entidad{
             //colocar instrucciones que siempre se deben hacer
         }
         return frm;
+    }
+
+/***********************    METODOS ADICIONALES   ***************************/
+
+    /**
+     * Completa el listado de formas segun el listado de aplicaciones que se le
+     * entrega. Cada aplicacion posee una forma y por cada una de ellas, se trae
+     * su configuracion, introduciendolo en el listado, asociandole el ID de la
+     * forma que le corresponde a la configuracion. Cada configuracion (Forma)
+     * esta compuesto por un listado de CampoForma.
+     * @param lstAplications
+     */
+    public void getFormasByAplications(List lstAplications ){
+
+        ConEntidad con = new ConEntidad();
+        if ((lstAplications!=null)&&(!lstAplications.isEmpty())){
+            Iterator it = lstAplications.iterator();
+            while (it.hasNext()){
+                Aplicacion apl = (Aplicacion) it.next();
+                Integer idForma = apl.getClaveFormaPrincipal();
+                String[] strData = new String[1];
+                strData[0]=String.valueOf(idForma);
+                List lstE = con.getListFormaById(strData);
+                addForma(idForma, lstE);
+            }
+        }
+    }
+
+    /**
+     * Entrega el ID de la forma asociada a la aplicacion, rescatado desde el
+     * listado de aplicaciones que le entrega el Perfil
+     * @param idAplication  ID de la aplicacion donde se va a buscar la forma
+     * @param lstAplications    Listado de aplicaciones, estas aplicaciones se
+     * deben obtener a travez del Perfil
+     * @return
+     */
+    public Integer getIdFormaByIdAplic(Integer idAplication, List lstAplications){
+        Integer idForma=Integer.valueOf(0);
+
+        if ((lstAplications!=null)&&(!lstAplications.isEmpty())){
+            Iterator it = lstAplications.iterator();
+            while (it.hasNext()){
+                Aplicacion apl = (Aplicacion) it.next();
+                if (idAplication.equals(apl.getClaveAplicacion())){
+                    idForma = apl.getClaveFormaPrincipal();
+                }
+            }
+        }
+        return idForma;
     }
 }
