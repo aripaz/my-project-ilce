9271cf2ec70d88c735e889644f8fe24c46596dc0
carlos.catrilef <carlos.catrilef@e6c9a562-5fef-0af6-cc1b-9fddd166eedc>
2011-04-27 21:48:56 +0000

    Se agrego metodo para poder ejecutar textos de queries sin necesidad que existe en la Base de Datos. Esta restringida a Select.

--- ProyILCE/src/java/mx/ilce/conection/ConQuery.java
+++ ProyILCE/src/java/mx/ilce/conection/ConQuery.java
@@ -189,6 +189,91 @@ class ConQuery {
     }
 
     /**
+     * Metodo para validar si la query entregada no posee alguna instruccion
+     * que permita la modificacion de la base de datos. Esto es una medida de
+     * seguridad, para que las queries de modificaccion solo sean las autorizadas,
+     * es decir, que provengan del catalogo de queries de la base de datos y no
+     * de una operacion externa.
+     * @param query     Query que debe ser validada
+     * @return
+     */
+    private boolean allowedQuery(String query){
+        boolean sld = true;
+        String str = query.toUpperCase();
+        if (str.contains("UPDATE")){
+            sld = false;
+        }else if (str.contains("DELETE")){
+            sld = false;
+        }else if (str.contains("INSERT")){
+            sld = false;
+        }else if (str.contains("DROP")){
+            sld = false;
+        }
+        return sld;
+    }
+
+    /**
+     * Obtiene la data aplicando la Query y Data entregada. Solo se permiten
+     * queries de seleccion de datos, si se envia alguna que posea instrucciones
+     * de modificacion o similar, se bloquea la operacion sin ejecutarla.
+     * @param query     Query que se debe ejecutar en la base de Datos
+     * @param arrData   Parametros con que se debe completar la query
+     * @return
+     * @throws SQLException
+     */
+    public HashCampo getDataByQuery(String query, String[] arrData) throws SQLException{
+        HashCampo hsCmp = new HashCampo();
+        try{
+            getConexion();
+            if (allowedQuery(query)){
+                if ((!"".equals(query)) && (arrData != null)){
+                    Statement ps = conn.createStatement();
+                    for(int i=1;i<=arrData.length;i++){
+                        String strData = arrData[i-1];
+                        if (strData != null){
+                            query = query.replaceFirst("%"+i, strData);
+                        }
+                    }
+                    ResultSet rs = ps.executeQuery(query);
+                    ResultSetMetaData rstm = rs.getMetaData();
+
+                    for (int i=1;i<=rstm.getColumnCount();i++){
+                        Campo cmp = new Campo(rstm.getColumnName(i),
+                                              Integer.valueOf(i),
+                                              rstm.getColumnTypeName(i));
+                        cmp.setTypeDataAPL(castTypeDataDBtoAPL(rstm.getColumnTypeName(i)));
+                        hsCmp.addCampo(cmp);
+                    }
+                    int i=0;
+                    while (rs.next()){
+                        List lstData = new ArrayList();
+                        List lstCampo = hsCmp.getListCampos();
+                        Iterator it = lstCampo.iterator();
+                        while (it.hasNext()){
+                            Campo itCmp = (Campo) it.next();
+                            Campo cmp = new Campo(itCmp.getNombre(),
+                                                  itCmp.getNombreDB(),
+                                                  itCmp.getCodigo(),
+                                                  itCmp.getTypeDataDB(),
+                                                  castTypeDataDBtoAPL(itCmp.getTypeDataDB()),
+                                                  getValueCampo(itCmp.getTypeDataDB(), rs, itCmp.getCodigo()));
+                            lstData.add(cmp);
+                        }
+                        hsCmp.addListData(lstData,i++);
+                    }
+                }
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }finally{
+            conn.close();
+        }
+        return hsCmp;
+    }
+
+    /**
      * Este metodo se debio utilizar, pero el PreparedStatement no esta
      * guardando las variables que se le entregan y deberia reemplazar en
      * las queries buscando el parametro con el signo de ?
@@ -253,7 +338,7 @@ class ConQuery {
     }
 
     /**
-     * Busca una query desde la base de datos, se le pasa el ID que posee
+     * Busca una query desde la base de datos, pasandole el ID que posee
      * en la tabla
      * @param idQuery
      * @return
@@ -278,7 +363,7 @@ class ConQuery {
     }
 
     /**
-     * Segun el tipo de dato que se le entrega, el cual viene desde la base de
+     * Segun el tipo de dato que se entrega, el cual viene desde la base de
      * datos, utiliza la conversion respectiva para dejar el valor como String
      * @param strType
      * @param rs
