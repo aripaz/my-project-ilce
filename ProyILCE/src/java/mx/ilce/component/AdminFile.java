0bdce2a7758790643a8107d7faad19801def7a48
carlos.catrilef <carlos.catrilef@e6c9a562-5fef-0af6-cc1b-9fddd166eedc>
2011-04-28 15:02:21 +0000

    Se implemento el manejo por archivo de properties la identificacion de las queries necesarias para el manejo de la aplicacion

--- ProyILCE/src/java/mx/ilce/component/AdminFile.java
+++ ProyILCE/src/java/mx/ilce/component/AdminFile.java
@@ -19,7 +19,41 @@ import java.util.Properties;
  */
 public class AdminFile {
     private static File WORKING_DIRECTORY;
-
+    /**
+     * Query para obtener datos del Usuario mediante User y password
+     */
+    public static String LOGIN = "LOGIN";
+    /**
+     * Query para obtener datos del Usuario mediante el User
+     */
+    public static String USER = "USER";
+    /**
+     * Query para obtener los datos del perfil
+     */
+    public static String PERFIL = "PERFIL";
+    /**
+     * Query para obtener los tab de aplicacion mediante la aplicacion y la
+     * forma
+     */
+    public static String TABFORMA = "TABFORMA";
+    /**
+     * Query para obtener los datos del XML de Session
+     */
+    public static String XMLSESSION = "XMLSESSION";
+    /**
+     * Query para obtener los datos del XML de menu (el acordeon)
+     */
+    public static String XMLMENU = "XMLMENU";
+    /**
+     * Query para obtener los datos de la forma
+     */
+    public static String FORMA = "FORMA";
+    /**
+     * Query para obtener los datos de la forma filtrando por un listado
+     * de campos
+     */
+    public static String FORMACAMPOS = "FORMACAMPOS";
+    
     /**
      * Lee la configuracion de la base de datos a utilizar presente en el
      * archivo ProyILCE.properties, ubicado en el directorio WEB-INF de la
@@ -57,6 +91,36 @@ public class AdminFile {
 	return prop;
     }
 
+    public static Properties leerIdQuery() throws Exception{
+        Properties prop = new Properties();
+	InputStream is = null;
+	File f = null;
+        File fichero = null;
+	try {
+            String separador = String.valueOf(File.separator);
+            URL url = AdminFile.class.getResource("AdminFile.class");
+
+            if(url.getProtocol().equals("file")) {
+		f = new File(url.toURI());
+		f = f.getParentFile().getParentFile();
+		f = f.getParentFile().getParentFile();
+		WORKING_DIRECTORY = f.getParentFile();
+            }
+            fichero = new File(WORKING_DIRECTORY + separador + "IdQuery.properties");
+            if (fichero.exists()){
+                is=new FileInputStream(fichero.getAbsolutePath());
+                prop.load(is);
+            }
+	} catch(IOException e) {
+            e.printStackTrace();
+	}finally{
+            if (is != null){
+                is.close();
+            }
+        }
+	return prop;
+    }
+
     /**
      * Obtiene el valor de una palabra clave (key), desde un arreglo de
      * properties
@@ -74,6 +138,21 @@ public class AdminFile {
 	}catch(Exception e){
             e.printStackTrace();
 	}
-            return sld;
+        return sld;
+    }
+
+    public Integer getIdQuery(Properties prop, String key){
+        Integer sld = Integer.valueOf(0);
+        String str = "";
+	try{
+            Enumeration e = prop.keys();
+            if (e.hasMoreElements()){
+                str = prop.getProperty(key);
+            }
+            sld = sld.valueOf(str);
+	}catch(Exception e){
+            e.printStackTrace();
+	}
+        return sld;
     }
 }
