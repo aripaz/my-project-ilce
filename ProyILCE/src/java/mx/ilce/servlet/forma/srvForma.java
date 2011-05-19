4aee84373ed9b73cd89cbd1b53fd69e33d6d1038
carlos.catrilef <carlos.catrilef@e6c9a562-5fef-0af6-cc1b-9fddd166eedc>
2011-05-19 23:12:18 +0000

    Se agrego el manejo de dato de entrada WHERE

--- ProyILCE/src/java/mx/ilce/servlet/forma/srvForma.java
+++ ProyILCE/src/java/mx/ilce/servlet/forma/srvForma.java
@@ -7,13 +7,20 @@ package mx.ilce.servlet.forma;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import mx.ilce.bean.Campo;
+import mx.ilce.bean.CampoForma;
+import mx.ilce.bean.HashCampo;
 import mx.ilce.component.AdminFile;
 import mx.ilce.component.AdminForm;
+import mx.ilce.conection.ConEntidad;
 import mx.ilce.controller.Forma;
 import mx.ilce.handler.ExceptionHandler;
  
@@ -36,24 +43,47 @@ public class srvForma extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
+            Forma forma = (Forma) request.getSession().getAttribute("forma");
             AdminForm admForm = new AdminForm();
             HashMap hs = admForm.getFormulario(request);
             HashMap hsForm = (HashMap) hs.get("FORM");  //Datos
             //HashMap hsFile = (HashMap) hs.get("FILE");  //Archivos
+            forma.setFormData(hsForm);
+            ArrayList arrayForm = (ArrayList) hs.get("arrayFORM");  //Datos
+            forma.setFormName(arrayForm);
 
             String claveForma = (String) hsForm.get("$cf");
             String pk = (String) hsForm.get("$pk");
             String tipoAccion = (String) hsForm.get("$ta");
+            String strWhere = (String) hsForm.get("$w");
 
-            Forma forma = (Forma) request.getSession().getAttribute("forma");
             if (forma !=null){
                 forma.setPk(pk);
                 forma.setClaveForma(Integer.valueOf(claveForma));
                 forma.setTipoAccion(tipoAccion);
-                if ("UPDATE".equals(((tipoAccion==null)?"":tipoAccion).trim().toUpperCase())){
-                    forma.setCleanIncrement(true);
+                if ((strWhere!=null)&&(!"".equals(strWhere))){
+                    String[] strData = new String[1];
+                    strData[0] = pk;
+                    forma.setArrayData(strData);
+                    String whereForm = getWhereData(hs, forma.getForma(forma.getClaveForma()));
+                    if ((strWhere!=null) && (strWhere.trim().length()>0)){
+                        if ((whereForm!=null) && (whereForm.trim().length()>0)){
+                            forma.setStrWhereQuery(strWhere + " AND " + whereForm);
+                        }else{
+                            forma.setStrWhereQuery(strWhere);
+                        }
+                    }else{
+                        if ((whereForm!=null) && (whereForm.trim().length()>0)){
+                            forma.setStrWhereQuery(whereForm);
+                        }
+                    }
+                    forma.ingresarBusquedaAvanzada();
+                }else{
+                    if ("UPDATE".equals(((tipoAccion==null)?"":tipoAccion).trim().toUpperCase())){
+                        forma.setCleanIncrement(true);
+                    }
+                    forma.mostrarForma();
                 }
-                forma.mostrarForma();
                 StringBuffer xmlForma = forma.getXmlEntidad();
                 request.getSession().setAttribute("xmlForma", xmlForma);
             }
@@ -80,6 +110,58 @@ public class srvForma extends HttpServlet {
         } finally {
             out.close();
         }
+    }
+
+    private String getWhereData(HashMap hsDataForm, List lstForma) throws ExceptionHandler{
+        String strSal = new String("");
+        try{
+            int numMaxParam = 10;
+            HashMap hsForm = (HashMap) hsDataForm.get("FORM");
+            StringBuffer str = new StringBuffer();
+
+            if ((lstForma != null) && (!lstForma.isEmpty())){
+                String[][] strCampos = new String[lstForma.size()][2];
+                StringBuffer strQuery = new StringBuffer("select ");
+                String nameTable ="";
+                Iterator it = lstForma.iterator();
+                int i=0;
+                while (it.hasNext()){
+                    CampoForma cmp = (CampoForma) it.next();
+                    String strData = (String) hsForm.get(cmp.getCampo());
+                    strCampos[i][0]= cmp.getCampo();
+                    strCampos[i++][1]= strData;
+                    nameTable = cmp.getTabla();
+                    strQuery.append(cmp.getCampo()).append(",");
+                }
+                strQuery.delete(strQuery.length()-1, strQuery.length());
+                strQuery.append(" from ").append(nameTable).append(" where 1=2");
+                ConEntidad con = new ConEntidad();
+                HashCampo hsCmp = con.getDataByQuery(strQuery.toString(), new String[0]);
+                if (hsCmp.getLengthCampo()>0){
+                    for (int j=0;j<i;j++){
+                        Campo cmp = hsCmp.getCampoByNameDB(strCampos[j][0]);
+                        if ( (strCampos[j][1]!=null) && (!"".equals(strCampos[j][1])) )  {
+                            if ("java.lang.String".equals(cmp.getTypeDataAPL()))
+                            {
+                                str.append(strCampos[j][0]).append(" = ").append(("'"+strCampos[j][1]+"'"));
+                            }else{
+                                str.append(strCampos[j][0]).append(" = ").append(strCampos[j][1]);
+                            }
+                            str.append(" AND ");
+                        }
+                    }
+                    if (str.length()>=4){
+                        if (str.substring(str.length()-4, str.length()).trim().equals("AND")) {
+                            str.delete(str.length()-4, str.length());    
+                        }
+                    }
+                }
+                strSal = str.toString();
+            }
+        }catch(Exception ex){
+            throw new ExceptionHandler(ex,this.getClass(),"Problemas para ejecutar la QUERY de la forma con el WHERE");
+        }
+        return strSal;
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
