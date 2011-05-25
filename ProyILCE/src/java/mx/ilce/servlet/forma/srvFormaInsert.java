dba9696f2da6f635b8ce6a75da89e124c6c801cb
carlos.catrilef <carlos.catrilef@e6c9a562-5fef-0af6-cc1b-9fddd166eedc>
2011-05-25 22:58:01 +0000

    Actualizacion de la documentacion de las clases

--- ProyILCE/src/java/mx/ilce/servlet/forma/srvFormaInsert.java
+++ ProyILCE/src/java/mx/ilce/servlet/forma/srvFormaInsert.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package mx.ilce.servlet.forma;
 
 import java.io.IOException;
@@ -26,12 +21,13 @@ import mx.ilce.util.Validation;
 /**
  * Servlet implementado para insertar los datos obtenidos de un formulario en
  * la Base de datos, asociado a la forma que le corresponda, segun el perfil
- * del usuario conectado. Utilizada en la creacion de datos nuevos
+ * del usuario conectado. Utilizada en la creacion de datos nuevos.
+ * Se modifico para tambien aceptar la modificacion de datos
  * @author ccatrilef
  */
 public class srvFormaInsert extends HttpServlet {
 
-    /** 
+    /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
@@ -44,82 +40,86 @@ public class srvFormaInsert extends HttpServlet {
         PrintWriter out = response.getWriter();
         Validation val = new Validation();
         try {
-            //Obtenemos los datos del formulario
-            AdminForm admF = new AdminForm();
-            HashMap hs = admF.getFormulario(request);
-
-            HashMap hsForm = (HashMap) hs.get("FORM");  //Datos
-            HashMap hsFile = (HashMap) hs.get("FILE");  //Archivos
-            HashMap hsFormQuery = new HashMap();
-
-            String claveForma = (String) hsForm.get("$cf");
-            String pk = (String) hsForm.get("$pk");
-            String tipoAccion = (String) hsForm.get("$ta");
-            
-            ArrayList arrVal = new ArrayList();
-            arrVal.add("$cf");
-            arrVal.add("$pk");
-            arrVal.add("$ta");
-
-            List lstVal = val.validationForm(arrVal, hsForm);
-            String blOK = (String) lstVal.get(0);
-            if ("false".equals(blOK)){
-                    val.executeErrorValidation(lstVal, this.getClass(), request, response);
+            if (!val.validateUser(request)){
+                val.executeErrorValidationUser(this.getClass(), request, response);
             }else{
-                Forma forma = (Forma) request.getSession().getAttribute("forma");
-                forma.setPk(pk);
-                forma.setClaveForma(Integer.valueOf(claveForma));
-                forma.setTipoAccion(tipoAccion);
-                List lstForma = forma.getForma(Integer.valueOf(claveForma));
-                List lstNew = null;
-                if (("0".equals(pk))&&("INSERT".equals(tipoAccion.toUpperCase()))){
-                    String[] arrayData = new String[2];
-                    arrayData[0] = claveForma;
-                    arrayData[1] = "insert";
-                    lstNew = forma.getNewFormaById(arrayData);
-                    if (!lstNew.isEmpty()){
-                        lstForma = lstNew;
-                        forma.addForma(Integer.valueOf(claveForma), lstNew);
-                    }
-                }
-                //Analizamos segun la forma obtenida
-                boolean obligatorioOk = true;
-                if ((!lstForma.isEmpty())&&(obligatorioOk)){
-                    Iterator it = lstForma.iterator();
-                    while ((it.hasNext())&&(obligatorioOk)){
-                        CampoForma cmp = (CampoForma) it.next();
-                        String dato = null;
-                        if (!"file".equals(cmp.getTipoControl())){
-                            dato = (String) hsForm.get(cmp.getCampo());
-                        }else{
-                            //Si es NULL, por el formato del formulario no se subio el archivo
-                            if (hsFile!=null){
-                                dato = (String) hsFile.get(cmp.getCampo());
-                            }
-                        }
-                        dato = val.replaceComillas(dato);
-                        hsFormQuery.put(cmp.getCampo(), dato);
-                    }
-                }
-                ExecutionHandler ex = new ExecutionHandler();
-                List lstData = new ArrayList();
-                if (obligatorioOk){
-                    lstData.add(hsFormQuery);
-                    lstData.add(forma);
-                    if ("0".equals(forma.getPk())){     // Es un nuevo elemento
-                        ex = forma.ingresarEntidad(lstData);
-                    }else{
-                        ex = forma.editarEntidad(lstData);
-                    }
-                    ex.setExecutionOK(true);
+                //Obtenemos los datos del formulario
+                AdminForm admF = new AdminForm();
+                HashMap hs = admF.getFormulario(request);
+
+                HashMap hsForm = (HashMap) hs.get("FORM");  //Datos
+                HashMap hsFile = (HashMap) hs.get("FILE");  //Archivos
+                HashMap hsFormQuery = new HashMap();
+
+                String claveForma = (String) hsForm.get("$cf");
+                String pk = (String) hsForm.get("$pk");
+                String tipoAccion = (String) hsForm.get("$ta");
+
+                ArrayList arrVal = new ArrayList();
+                arrVal.add("$cf");
+                arrVal.add("$pk");
+                arrVal.add("$ta");
+
+                List lstVal = val.validationForm(arrVal, hsForm);
+                String blOK = (String) lstVal.get(0);
+                if ("false".equals(blOK)){
+                        val.executeErrorValidation(lstVal, this.getClass(), request, response);
                 }else{
-                    //Si hubo falla se eliminan los archivo subidos
-                    ex.setExecutionOK(false);
-                    AdminFile.deleFileFromServer(hsFile);
+                    Forma forma = (Forma) request.getSession().getAttribute("forma");
+                    forma.setPk(pk);
+                    forma.setClaveForma(Integer.valueOf(claveForma));
+                    forma.setTipoAccion(tipoAccion);
+                    List lstForma = forma.getForma(Integer.valueOf(claveForma));
+                    List lstNew = null;
+                    if (("0".equals(pk))&&("INSERT".equals(tipoAccion.toUpperCase()))){
+                        String[] arrayData = new String[2];
+                        arrayData[0] = claveForma;
+                        arrayData[1] = "insert";
+                        lstNew = forma.getNewFormaById(arrayData);
+                        if (!lstNew.isEmpty()){
+                            lstForma = lstNew;
+                            forma.addForma(Integer.valueOf(claveForma), lstNew);
+                        }
+                    }
+                    //Analizamos segun la forma obtenida
+                    boolean obligatorioOk = true;
+                    if ((!lstForma.isEmpty())&&(obligatorioOk)){
+                        Iterator it = lstForma.iterator();
+                        while ((it.hasNext())&&(obligatorioOk)){
+                            CampoForma cmp = (CampoForma) it.next();
+                            String dato = null;
+                            if (!"file".equals(cmp.getTipoControl())){
+                                dato = (String) hsForm.get(cmp.getCampo());
+                            }else{
+                                //Si es NULL, por el formato del formulario no se subio el archivo
+                                if (hsFile!=null){
+                                    dato = (String) hsFile.get(cmp.getCampo());
+                                }
+                            }
+                            dato = val.replaceComillas(dato);
+                            hsFormQuery.put(cmp.getCampo(), dato);
+                        }
+                    }
+                    ExecutionHandler ex = new ExecutionHandler();
+                    List lstData = new ArrayList();
+                    if (obligatorioOk){
+                        lstData.add(hsFormQuery);
+                        lstData.add(forma);
+                        if ("0".equals(forma.getPk())){     // Es un nuevo elemento
+                            ex = forma.ingresarEntidad(lstData);
+                        }else{
+                            ex = forma.editarEntidad(lstData);
+                        }
+                        ex.setExecutionOK(true);
+                    }else{
+                        //Si hubo falla se eliminan los archivo subidos
+                        ex.setExecutionOK(false);
+                        AdminFile.deleFileFromServer(hsFile);
+                    }
+                    Integer xml = (Integer) ((ex.getObjectData()==null)?new Integer(0):ex.getObjectData());
+                    request.getSession().setAttribute("xmlTab", String.valueOf(xml));
+                    request.getRequestDispatcher("/resource/jsp/xmlTab.jsp").forward(request, response);
                 }
-                Integer xml = (Integer) ((ex.getObjectData()==null)?new Integer(0):ex.getObjectData());
-                request.getSession().setAttribute("xmlTab", String.valueOf(xml));
-                request.getRequestDispatcher("/resource/jsp/xmlTab.jsp").forward(request, response);
             }
         }catch (ExceptionHandler eh){
             try{
@@ -137,7 +137,7 @@ public class srvFormaInsert extends HttpServlet {
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
      * @param request servlet request
      * @param response servlet response
@@ -148,9 +148,9 @@ public class srvFormaInsert extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
      * @param request servlet request
      * @param response servlet response
@@ -163,7 +163,7 @@ public class srvFormaInsert extends HttpServlet {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
      * @return a String containing servlet description
      */
