1d653d74b89efda768007898252e1461ee623141
carlos.catrilef <carlos.catrilef@e6c9a562-5fef-0af6-cc1b-9fddd166eedc>
2011-06-21 19:47:24 +0000

    Nueva clase definida para cambios

---
+++ ProyILCE/src/java/mx/ilce/bean/BIT.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mx.ilce.bean;
+
+/**
+ *
+ * @author ccatrilef
+ */
+public class BIT {
+
+    Integer integ = new Integer(0);
+
+    //integ.valueOf(null);
+    //integ.getInteger(null);
+
+
+}
